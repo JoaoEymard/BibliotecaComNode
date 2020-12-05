bad4bd671ef042935f3d039d0148bb4ba6ea7f48
joaoeymard <eymarduarte12@hotmail.com>
2020-12-05 18:48:26 -0300

    Uma correção no get postagem

--- src/routes/teste.js
+++ src/routes/teste.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const kenx = require('../database/db_config')
+const knex = require('../database/db_config')
 
 
 const rotas = Router();
@@ -10,10 +10,25 @@ rotas.get('/teste',(req,res)=>{
     })
 });
 
-rotas.post('/postagem/:id',(req,res)=>{
-    const {id} = req.params;
-    return res.status(200).json(req.body);
-   //return res.status(200).json({"idPost":id});
+/**
+ * Rota para pegar uma postagem, baseada no ID
+ */
+rotas.get('/postagem/:id', async (req,res) => {
+    try {
+        
+        // Usa o knex para fazer select no banco que já está conectado.
+        const rows = await knex
+            .select('*')
+            .from('table')
+            .where(req.params); // Passa o objeto params por parametro no where
+    
+        // Retorna a requisição com sucesso
+        return res.status(200).json(rows);
+
+    } catch (error) {
+        // Se tiver algum erro durante a execução do código acima, envia a msg do erro para o client
+        res.status(500).json({ message: error.message })
+    }
 })
 
 module.exports = rotas;
